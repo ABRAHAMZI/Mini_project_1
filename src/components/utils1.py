3797a9a14a44d84690faf8553eef15c307d7ac08
Abraham_Ziegen <abrahamziegen@gmail.com>
2024-05-22 00:38:29 +0530

    grid_search_updated

--- src/components/utils1.py
+++ src/components/utils1.py
@@ -10,6 +10,7 @@ import pandas as pd
 from exception1 import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -23,18 +24,18 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, Y_train,X_test,Y_test,models):
+def evaluate_models(X_train, Y_train,X_test,Y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,Y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
 
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,Y_train)
 
             #model.fit(X_train, y_train)  # Train model
